98e854a0813c11759c3ce32b946419d4d7494135
Kamlesh Agrahari <133155005+Kamleshagrahari@users.noreply.github.com>
2024-08-27 17:00:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Help Classmates/help-classmates.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*; 
+import java.io.*; 
+
+class GFG 
+{ 
+	public static void main (String[] args) { 
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            int array[] = new int[n];
+            for (int i = 0; i < n; ++i)
+            {
+                array[i] = sc.nextInt();
+            }
+
+            Solution ob = new Solution();
+
+            int ans[] = ob.help_classmate(array,n);
+
+           	for (int i=0; i<n; i++) 
+                System.out.print(ans[i]+" "); 
+            System.out.println();
+            t--;
+        }
+	} 
+} 
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+	public static int[] help_classmate(int arr[], int n) 
+	{ 
+	    // Your code goes here
+	    int res[]=new int[n];
+	    for(int i=0;i<n;i++){
+	        res[i]=-1;
+	        for(int j=i+1;j<n;j++){
+	            if(arr[i]>arr[j]){
+	                res[i]=arr[j];
+	                break;
+	            }
+	        }
+	        
+	    }
+	    return res;
+	} 
+}
